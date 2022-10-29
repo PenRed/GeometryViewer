e7765d50241b328cd8ac382eced20bd84c64789b
Vicent Giménez Alventosa <vicent.gimenez.alventosa@gmail.com>
2022-10-30 00:02:28 +0200

    Add files via upload

--- GeometryViewer.sh
+++ GeometryViewer.sh
@@ -7,9 +7,9 @@ tmp="${dirname#?}"
 if [ "${dirname%$tmp}" != "/" ]; then
 dirname=$PWD/$dirname
 fi
-LD_LIBRARY_PATH=$dirname
+LD_LIBRARY_PATH=${dirname}/lib
 export LD_LIBRARY_PATH
 
-echo $LD_LIBRARY_PATH
+echo "Path lib: ${LD_LIBRARY_PATH}"
 
 $dirname/$appname "$@"
